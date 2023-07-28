bb3854b41833c8673c164336e84f7eb177f67091
3Harang <jungjaeseung9475@gmail.com>
2023-07-28 10:03:35 +0900

    navbar hamburger menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { AiOutlineMenu } from "react-icons/ai";
+import { useCallback, useState } from "react";
+
 import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -14,7 +23,7 @@ const UserMenu = () => {
           어디든 편하게 예약하기
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-t-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
@@ -23,6 +32,16 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="로그인" />
+              <MenuItem onClick={() => {}} label="회원가입" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
